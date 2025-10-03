d3bf45b572e781855374f2131f249f6e42599f57
Wellinton <wellintongerson@hotmail.com>
2025-10-03 05:56:04 -0300

    Entrega do desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("(81)99887-1360","Sansung galaxy 21", "1874", 240);
+
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Temple Run");
+
+Console.WriteLine("\n");
+
+Iphone i1 = new Iphone("(81)99887-1350","Iphone 10", "1844", 120);
+
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("League of Legends");
